65ff7a563d226e82d1cf4d41e23882a22304e449
ErwinDai <106577401+lostdyy@users.noreply.github.com>
2024-06-29 11:35:20 +0800

    Delete dai03 directory

--- dai03/Sat.m
+++
@@ -1,8 +0,0 @@
-function [ phi ] = Sat( r,sigma)
-%UNTITLED3 此处显示有关此函数的摘要
-%   此处显示详细说明
-
-phi = sign(r)*min(abs(r),sigma);
-
-end
-
--- dai03/f.m
+++
@@ -1,8 +0,0 @@
-function [ fx ] = f( x )
-%UNTITLED2 此处显示有关此函数的摘要
-%   此处显示详细说明
-  fx(1,:) = 0.3*sin(x(1));
-  fx(2,:) = 0.2*sin(x(2));
-
-end
-
